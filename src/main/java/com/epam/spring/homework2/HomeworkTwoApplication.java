59db33db7815219eb5bdb9d1c34be75ee4e25ece
Dmytro Novytskyi <dima.lyulka@gmail.com>
2022-06-25 00:04:22 +0300

    Completed tasks

---
+++ src/main/java/com/epam/spring/homework2/HomeworkTwoApplication.java
@@ -0,0 +1,27 @@
+package com.epam.spring.homework2;
+
+import com.epam.spring.homework2.beans.BeanMaster;
+import com.epam.spring.homework2.config.FirstConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class HomeworkTwoApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(FirstConfig.class);
+        System.out.println("------------------------------------");
+        for (String s :
+                context.getBeanDefinitionNames()) {
+            System.out.println(s);
+        }
+        System.out.println("------------------------------------");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .map(context::getBean)
+                .filter(b -> b instanceof BeanMaster)
+                .forEach(System.out::println);
+        System.out.println("------------------------------------");
+        context.close();
+    }
+
+}
---
+++ src/main/java/com/epam/spring/homework2/beans/BeanC.java
@@ -0,0 +1,17 @@
+package com.epam.spring.homework2.beans;
+
+public class BeanC extends BeanMaster{
+
+    public BeanC(String name, int value) {
+        super(name, value);
+    }
+
+    public void initCheck(){
+        System.out.println("BeanC -> initMethod by @Bean(initMethod = {})");
+    }
+
+    public void destroyCheck(){
+        System.out.println("BeanC -> destroyMethod by @Bean(destroyMethod = {})");
+    }
+
+}
